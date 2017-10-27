3da9a3d126f49648e6800998307424dda90fc651
US\vanessa.pavia <vanessa.pavia@accenturefederal.com>
2017-10-27 16:07:54 -0400

    Adding R file

---
+++ vanessa/r/P1.R
@@ -0,0 +1,31 @@
+x <- c(1,2,3,4,5)
+
+sum = 0 
+for (i in x) {sum = sum + 1}
+return (sum)
+
+
+
+prime_check<- c(14){
+
+if (prime_check <= 0)
+  return(FALSE)
+if( prime_check >1)
+  for (i in 1:prime_check)
+    if (prime_check(vector)%% i == 0)
+    return(FALSE)}
+else {return(TRUE)}
+
+
+
+sq <- c()
+j = sq ^ 0.5
+return()
+
+
+
+GCf <- c(a,b){
+if (GCF[2]==0){return(GCF[1])}
+else 
+  return c(GCF[2], GCF[1]%%GCF[2])}
+
