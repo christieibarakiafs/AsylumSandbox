14aeaabad23a4806fdfd70f4834dedda26356c63
US\vanessa.pavia <vanessa.pavia@accenturefederal.com>
2017-10-31 09:40:03 -0400

    Updating R file

--- vanessa/r/P1.R
+++ vanessa/r/P1.R
@@ -1,31 +1,41 @@
-x <- c(1,2,3,4,5)
+add <- function(x){
+  bucket = 0
+  for(i in x){
+    bucket = bucket + i
+  }
+  return(bucket)
+}
 
-sum = 0 
-for (i in x) {sum = sum + 1}
-return (sum)
+prime_check<- function(x){
+
+  if (x <= 1) {
+    return(FALSE)
+  }
+  
+  if( x >1){
+    for (i in 2:(x-1)){
+      if( x %% i == 0){
+        return(FALSE)
+      }
+    }
+    return(TRUE)
+}
+}
+
+
+square <- function(x){
+j = x ^ 0.5
+  return(j)}
 
 
 
-prime_check<- c(14){
+GCF <- function(x,y){
+  if (y ==0 ){
+    return(x)
+  }
+  else {
+    return(GCF(y,x%%y))
+  }
+}
 
-if (prime_check <= 0)
-  return(FALSE)
-if( prime_check >1)
-  for (i in 1:prime_check)
-    if (prime_check(vector)%% i == 0)
-    return(FALSE)}
-else {return(TRUE)}
-
-
-
-sq <- c()
-j = sq ^ 0.5
-return()
-
-
-
-GCf <- c(a,b){
-if (GCF[2]==0){return(GCF[1])}
-else 
-  return c(GCF[2], GCF[1]%%GCF[2])}
 
